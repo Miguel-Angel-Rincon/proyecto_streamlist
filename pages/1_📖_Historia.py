1e42982703682ca0f6a89908e6471fa265eced49
Miguel-Angel-Rincon <169084478+Miguel-Angel-Rincon@users.noreply.github.com>
2025-09-17 12:02:10 -0500

    mm

--- pages/1_📖_Historia.py
+++ pages/1_📖_Historia.py
@@ -2,7 +2,7 @@
 
 import streamlit as st
 
-st.set_page_config(page_title="Historia del Baloncesto", layout="wide")
+st.set_page_config(page_title="📖Historia del Baloncesto", layout="wide")
 
 # ====== Estilos ======
 st.markdown("""
